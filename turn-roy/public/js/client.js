5adb1af1ea31b426d9f715df77665626303879b8
Brandon Schmeidler <brandon.bsgstudio@gmail.com>
2021-10-17 22:13:03 -0500

    put a grid on the screen and center the camera so we can see everything better

--- turn-roy/public/js/client.js
+++ turn-roy/public/js/client.js
@@ -13,8 +13,10 @@ var game = new Phaser.Game(config);
 var currentScene;
 var socket;
 
+const GRID_SIZE = 11;
 const CELL_SIZE = 32;
-var playerSprites = {}
+
+var playerSprites = {};
 
 function preload () {
     this.load.spritesheet("tileset","../assets/tileset.png",{frameWidth:CELL_SIZE,frameHeight:CELL_SIZE});
@@ -22,6 +24,10 @@ function preload () {
 
 function create() {
     currentScene = this;
+
+    centerCamera();
+    spawnGrid();
+
     socket = io();
 
     socket.on("spawn_players",(clients) => {
@@ -35,6 +41,22 @@ function create() {
 
 function update() {
 
+}
+
+function centerCamera() {
+    var gridBound = GRID_SIZE * CELL_SIZE;
+    var cx = (CELL_SIZE / 2) + ((config.width - gridBound) / 2);
+    var cy = (CELL_SIZE / 2) + ((config.height - gridBound) / 2);
+    
+    currentScene.cameras.main.setBounds(-cx,-cy,config.width,config.height);
+}
+
+function spawnGrid() {
+    for (var i = 0; i < GRID_SIZE; ++i) {
+        for (var j = 0; j < GRID_SIZE; ++j) {
+            currentScene.add.sprite(i * CELL_SIZE, j * CELL_SIZE, "tileset", 0);
+        }
+    }
 }
 
 function spawnPlayers(clients) {
